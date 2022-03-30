1d50da378ba7edda0bcd869003556bf6a57f98fa
unknown <rahulmurugesh.work@gmail.com>
2022-03-30 23:25:58 +0530

    updated notes component to display notes using map function

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Note from "./components/note";
-
+import notes from "./components/notes";
 
 function App() {
     return (
         <div>
             <Header />
-            <Note className="note" />
+            {notes.map(noteItem => <Note 
+                key = {noteItem.key}
+                title = {noteItem.title}
+                content = {noteItem.content}
+                />
+            )}
+
             <Footer />
         </div>
     );
