006bb642ec8b4078c5003d80f00daa593c13bb46
unknown <rahulmurugesh.work@gmail.com>
2022-04-02 23:43:07 +0530

    added delete functionality to Note component

--- src/app.jsx
+++ src/app.jsx
@@ -9,17 +9,28 @@ function App() {
 
     function addNote(note) {
         setNotesList(prevNote => [...prevNote,note])
-        }
+    }
+    
+    function deleteNote(id) {
+        setNotesList(prevNotes => {
+            return(
+                prevNotes.filter((note,index) => index !== id)
+            )
+        })
+    }
+    
     
    return (
         <div>
             <Header />
             <CreateArea onAdd={addNote} />
 
-            {notesList.map(note => <Note 
-                key = {note.key}
+            {notesList.map((note,index) => <Note 
+                key = {index}
+                id = {index}
                 title = {note.title}
                 content = {note.content}
+                onRemove = {deleteNote}
                 />
             )}
 
